efe22652060aa64ff4875c1824b9fa5309eb9a50
JMEDINILHA <hae.joao.lmedinilha@hyundai-brasil.com>
2023-06-02 19:59:49 -0300

    rota login

--- services/routers/routers.js
+++ services/routers/routers.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 var mysql = require('mysql')
-
+const crypto = require('crypto')
 
 const connection = mysql.createConnection({
   host: process.env.DB_IP,
@@ -21,13 +21,26 @@ connection.connect((err) => {
 router.get('/userList', function (req, res, next) {
   connection.query('SELECT name, email FROM users;', function (err, rows) {
     if (err) {
-      res.status(500).json({ STATUS: "500", CODE: "E", MSG: "common.error", RETURN: null })
+      console.log(err)
+      res.status(500).json({ STATUS: "500", CODE: "E", MSG: "common.error", RETURN: err })
     } else {
       res.status(200).json({ STATUS: "200", CODE: "S", MSG: "common.success", RETURN: rows[0] })
     }
   })
 })
 
+router.get('/login', function (req, res, next) {
+  const hash = crypto.createHash('sha256').update(req.query.pw).digest('hex');
+  connection.query(`SELECT COUNT(*) as CNT FROM users WHERE username = '${req.query.username}' AND password = '${hash}'; `, function (err, rows) {
+    if (err) {
+      console.log(err)
+      res.status(500).json({ STATUS: "500", CODE: "E", MSG: "common.error", RETURN: err })
+    } else {
+      console.log(rows)
+      res.status(200).json({ STATUS: "200", CODE: "S", MSG: "common.success", RETURN: rows[0] })
+    }
+  })
+})
 
 
 module.exports = router
